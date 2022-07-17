3a7a915b12c24a16d880e8be35f5084fcb56d001
msynk <msynk@outlook.com>
2022-07-17 23:55:46 +0430

    add projects

---
+++ src/RazorClassLibrary1/Bounds.cs
@@ -0,0 +1,7 @@
+﻿namespace RazorClassLibrary1;
+
+public class Bounds
+{
+    public double Width { get; set; }
+    internal double Height { get; set; }
+}
---
+++ src/RazorClassLibrary1/ExampleJsInterop.cs
@@ -0,0 +1,43 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace RazorClassLibrary1;
+
+// This class provides an example of how JavaScript functionality can be wrapped
+// in a .NET class for easy consumption. The associated JavaScript module is
+// loaded on demand when first needed.
+//
+// This class can be registered as scoped DI service and then injected into Blazor
+// components for use.
+
+public class ExampleJsInterop : IAsyncDisposable
+{
+    private readonly Lazy<Task<IJSObjectReference>> moduleTask;
+
+    public ExampleJsInterop(IJSRuntime jsRuntime)
+    {
+        moduleTask = new(() => jsRuntime.InvokeAsync<IJSObjectReference>(
+            "import", "./_content/RazorClassLibrary1/exampleJsInterop.js").AsTask());
+    }
+
+    public async ValueTask<string> Prompt(string message)
+    {
+        var module = await moduleTask.Value;
+        return await module.InvokeAsync<string>("showPrompt", message);
+    }
+
+    internal async ValueTask<Bounds> GetBoundingClientRect(ElementReference element)
+    {
+        var module = await moduleTask.Value;
+        return await module.InvokeAsync<Bounds>("getBoundingClientRect", element);
+    }
+
+    public async ValueTask DisposeAsync()
+    {
+        if (moduleTask.IsValueCreated)
+        {
+            var module = await moduleTask.Value;
+            await module.DisposeAsync();
+        }
+    }
+}
